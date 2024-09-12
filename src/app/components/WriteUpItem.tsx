9b988b13c907a3a42a17d7e98cae6ae18314cd3f
BluTargaryan <opeyemiyolo@gmail.com>
2024-09-12 15:06:13 +0100

    fixed line clamp in WriteUpItem code

--- src/app/components/WriteUpItem.tsx
+++ src/app/components/WriteUpItem.tsx
@@ -6,13 +6,15 @@ const WriteUpItem = () => {
     <div className='w-full h-60 bg-cover bg-center flex flex-col justify-end'
     style={{backgroundImage:`url('https://images.unsplash.com/photo-1532630571098-79a3d222b00d?q=80&w=1872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`}}
     >
-       <div className='w-full h-14 text-selfPrimary bg-selfSecondary flex flex-col items-start justify-center px-2 transition-all
+       <div className='w-full h-14 text-selfPrimary bg-selfSecondary flex flex-col items-start justify-center px-2 gap-1 transition-all
        hover:h-full group
        '>
         <h2>Gaming as a subculture</h2>
         <p className='text-xs'>Gaming</p>
-        <p className='text-sm text-left hidden group-hover:block border'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw
+        <p className='text-sm text-left hidden w-full group-hover:line-clamp-3'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor 
+        sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw Lorem ipsum dolor sit amet, consectetur adipiscing elit,xsxsssxxxxxxxx swddwd  wdwdwdwdw
         </p>
         </div> 
     </div>
